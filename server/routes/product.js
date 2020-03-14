a0d0f7a2f145c8d83b29fb4d82958758fe0117ee
maksalizm <maksalizm@gmail.com>
2020-03-14 19:38:17 +0900

    more product data

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 
-const { Product } = require("../models/Product");
-const { auth } = require("../middleware/auth");
+const {Product} = require("../models/Product");
+const {auth} = require("../middleware/auth");
 
 //=================================
 //             Product
@@ -33,7 +33,7 @@ router.post("/uploadImage", auth, (req, res) => {
   //Multer library
   
   upload(req, res, err => {
-    if(err) return res.json({success: false, err});
+    if (err) return res.json({success: false, err});
     return res.json({success: true, image: res.req.file.path, fileName: res.req.file.filename})
   })
 });
@@ -43,20 +43,29 @@ router.post("/uploadProduct", auth, (req, res) => {
   // save all the data we got from the client into the DB
   const product = new Product(req.body);
   product.save((err) => {
-    if(err) return res.status(400).json({success: false, err})
+    if (err) return res.status(400).json({success: false, err})
     return res.status(200).json({success: true})
   })
   
 });
 
-router.get("/getProducts", auth, (req, res) => {
+router.post("/getProducts", auth, (req, res) => {
+  
+  let order = req.body.order ? req.body.order : "desc";
+  let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+  
   Product.find()
-    .exec((err, products) => {
-      if(err) return res.status(400).json({success: false, err})
-      res.status(200).json({success: true, products})
-    })
+         .populate("writer")
+         .sort([[sortBy, order]])
+         .skip(skip)
+         .limit(limit)
+         .exec((err, products) => {
+           if (err) return res.status(400).json({success: false, err});
+           res.status(200).json({success: true, products, postSize: products.length})
+         })
 });
-
 
 
 module.exports = router;
