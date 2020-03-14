7d21e81738453f4aafaa861500452e408f3a100b
maksalizm <maksalizm@gmail.com>
2020-03-15 04:32:06 +0900

    radio

--- server/routes/product.js
+++ server/routes/product.js
@@ -60,7 +60,10 @@ router.post("/getProducts", (req, res) => {
   for (let key in req.body.filters) {
     if(req.body.filters[key].length) {
       if(key === "price") {
-      
+        findArgs[key] = {
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1]
+        }
       } else {
         findArgs[key] = req.body.filters[key];
       }
